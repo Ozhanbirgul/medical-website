83289e521ff88e69e7c9b56630eb15698240f45b
Birgül Özhan <birgulozhan9@gmail.com>
2024-06-14 23:40:17 +0300

    güncelleme

--- js/app.js
+++ js/app.js
@@ -59,7 +59,40 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    var reviewSwiper = new Swiper('.reviews.swiper', {
+    var reviewSwiper = new Swiper('.reviews', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        slidePerGroup: 4,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        loop: false 
+    });
+
+    // İleri ve geri oklara tıklama olayı eklemek için
+    var nextButton = document.querySelector('.swiper-button-next');
+    var prevButton = document.querySelector('.swiper-button-prev');
+
+    nextButton.addEventListener('click', function() {
+        reviewSwiper.slideNext(); // İleri gitmek için
+    });
+
+    prevButton.addEventListener('click', function() {
+        reviewSwiper.slidePrev(); // Geri gitmek için
+    });
+
+
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var newSwiper = new Swiper('.swiper-container', {
         slidesPerView: 4,
         spaceBetween: 30,
         slidePerGroup: 4,
@@ -92,36 +125,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    var newsSwiper = new Swiper('.news.swiper', {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        slidesPerGroup: 4,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.news-swiper-button-next', // İleri düğmesinin sınıf adı başında nokta olmalı
-            prevEl: '.news-swiper-button-prev', // Geri düğmesinin sınıf adı başında nokta olmalı
-        },
-    });
 
-    // İleri ve geri oklara tıklama olayı eklemek için
-    var nextButton = document.querySelector('.news-swiper-button-next');
-    var prevButton = document.querySelector('.news-swiper-button-prev');
-
-    nextButton.addEventListener('click', function() {
-        newsSwiper.slideNext(); // İleri gitmek için
-    });
-
-    prevButton.addEventListener('click', function() {
-        newsSwiper.slidePrev(); // Geri gitmek için
-    });
-
-});
 
 
 
